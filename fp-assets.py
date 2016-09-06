aa23b4fe909931ce1dacc5b16ab25981ccbbb392
snickerbockers <chimerasaurusrex@gmail.com>
2016-09-06 09:26:22 -0700

    Moved rebuild.py's functionality into fp-assets.py and changed command-line arguments to match tar more closely

--- fp-assets.py
+++ fp-assets.py
@@ -9,6 +9,7 @@
 #
 ################################################################################
 
+import re
 import struct
 import os
 import zlib
@@ -28,13 +29,6 @@ TYPE_SIZE_COUNT = 5
 assets_file_path="Assets.dat"
 assets_dir_path="Assets"
 
-img_offsets = []
-sound_offsets = []
-font_offsets = []
-shader_offsets = []
-file_offsets = []
-type_sizes = []
-
 img_dir = None
 audio_dir = None
 shader_dir = None
@@ -45,12 +39,16 @@ preload_file_path = None
 type_sizes_path = None
 
 usage_string = """\
-Usage: %s [ -f|--in-file=<in-file> ] [ -d|--out-dir=<out-dir> ]
+Usage: %s -c | -x [ -f|--file=<in-file> ] [pathname]
 
-in_file is a path to your Assets.dat file.  it defaults to ./Assets.dat
-out_dir is a path to where you want the Assets to go.  It defaults to ./Assets
+in-file is a path to your Assets.dat file.  it defaults to ./Assets.dat
+pathname is the path to the directory to be extracted to/created from.
+    It defaults to ./Assets/
 
-This script will exit with an error if out_dir already exists.
+-c creates a new Assets.dat.
+-x extracts Assets.dat
+
+extracting will exit with an error if pathname already exists.
 """ % sys.argv[0]
 
 def extract_glyph(assets_file, metrics_path, img_path):
@@ -174,7 +172,172 @@ def init_paths(assets_dir_path):
     preload_file_path = os.path.join(assets_dir_path, "preload_data.bin")
     type_sizes_path = os.path.join(assets_dir_path, "type_sizes.txt")
 
+def write_glyph(assets_file, img_path, metrics_path):
+    metrics_file = open(metrics_path, "r")
+
+    metrics_data_map = json.loads(metrics_file.read())
+    metrics_data_bin = struct.pack("<IffffffffII",                       \
+                                   int(metrics_data_map['charcode']),    \
+                                   float(metrics_data_map['x1']),        \
+                                   float(metrics_data_map['y1']),        \
+                                   float(metrics_data_map['x2']),        \
+                                   float(metrics_data_map['y2']),        \
+                                   float(metrics_data_map['advance_x']), \
+                                   float(metrics_data_map['advance_y']), \
+                                   float(metrics_data_map['corner_x']),  \
+                                   float(metrics_data_map['corner_y']),  \
+                                   int(metrics_data_map['width']),       \
+                                   int(metrics_data_map['height']))
+
+    assets_file.write(metrics_data_bin)
+
+    w = metrics_data_map['width']
+    h = metrics_data_map['height']
+
+    if w > 0 and h > 0:
+        img = Image.open(img_path)
+        assets_file.write(img.tobytes())
+
+def write_font(assets_file, cur_font_dir):
+    font_meta_file = open(os.path.join(cur_font_dir, \
+                                       "font_metrics.json"), "r")
+    metrics_data_map = json.loads(font_meta_file.read())
+    metrics_data_bin = struct.pack("<HHffffI",                         \
+                                   int(metrics_data_map['size']),      \
+                                   int(metrics_data_map['flags']),     \
+                                   float(metrics_data_map['width']),   \
+                                   float(metrics_data_map['height']),  \
+                                   float(metrics_data_map['ascent']),  \
+                                   float(metrics_data_map['descent']), \
+                                   int(metrics_data_map['glyph_count']))
+    assets_file.write(metrics_data_bin)
+    for glyph_no in range(metrics_data_map['glyph_count']):
+        metrics_path = os.path.join(cur_font_dir, \
+                                    "glyph_%d_metrics.json" % glyph_no)
+        img_path = os.path.join(cur_font_dir, "glyph_%d.png" % glyph_no)
+        write_glyph(assets_file, \
+                    metrics_path=metrics_path, img_path=img_path)
+
+def write_img(assets_file, img_path, meta_path):
+    img = Image.open(img_path, "r")
+    img_w, img_h = img.size
+    data = zlib.compress(img.tobytes(), 9)
+    img_meta_file = open(meta_path, "r")
+    img_meta_txt = img_meta_file.read().splitlines()
+    img_meta_data = struct.pack("<HHHH", \
+                                int(img_meta_txt[0], 0), \
+                                int(img_meta_txt[1], 0), \
+                                int(img_meta_txt[2], 0), \
+                                int(img_meta_txt[3], 0))
+    assets_file.write(struct.pack("<HH", img_w, img_h))
+    assets_file.write(img_meta_data)
+    assets_file.write(struct.pack("<I", len(data)))
+    assets_file.write(data)
+
+def write_sound(assets_file, sound_path, meta_path):
+    sound_meta_file = open(meta_path, "r")
+    sound_meta_txt = sound_meta_file.read().splitlines()
+    sound_meta_data = struct.pack("BBBB",
+                                  int(sound_meta_txt[0], 0), \
+                                  int(sound_meta_txt[1], 0), \
+                                  int(sound_meta_txt[2], 0), \
+                                  int(sound_meta_txt[3], 0))
+    sound_file = open(sound_path, "r")
+    sound_data = sound_file.read()
+
+    assets_file.write(sound_meta_data)
+    assets_file.write(struct.pack("<I", len(sound_data)))
+    assets_file.write(sound_data)
+
+def write_text(assets_file, text_file_path):
+    text_file = open(text_file_path, "r")
+    text_data = text_file.read()
+    text_len = struct.pack("<I", len(text_data))
+    assets_file.write(text_len)
+    assets_file.write(text_data)
+
+def write_assets_file(assets_file_path, assets_dir_path):
+    init_paths(assets_dir_path)
+    assets_file = open(assets_file_path, "wb")
+
+    preload_file = open(preload_file_path, "rb")
+    preload_data = preload_file.read()
+    preload_file.close()
+
+    assets_file.write(preload_data)
+    assert assets_file.tell() == OFFSETS_START
+
+    offset_block_size = 4 * (IMG_COUNT + SOUND_COUNT + FONT_COUNT \
+                             + SHADER_COUNT + FILE_COUNT + TYPE_SIZE_COUNT)
+    assets_file.seek(OFFSETS_START + offset_block_size, os.SEEK_SET)
+
+    img_offsets = []
+    for img_idx in range(IMG_COUNT):
+        img_offsets.append(assets_file.tell())
+
+        write_img(assets_file, \
+                  img_path=os.path.join(img_dir, "img_%d.png" % img_idx), \
+                  meta_path=os.path.join(img_dir, "img_%d_meta.txt" % img_idx))
+
+    sound_offsets = []
+    for sound_idx in range(SOUND_COUNT):
+        sound_offsets.append(assets_file.tell())
+
+        sound_path = os.path.join(audio_dir, "audio_%d.ogg" % sound_idx)
+        meta_path = os.path.join(audio_dir, "audio_%d_meta.txt" % sound_idx)
+        write_sound(assets_file, sound_path=sound_path, meta_path=meta_path)
+
+    font_offsets = []
+    for font_idx in range(FONT_COUNT):
+        font_offsets.append(assets_file.tell())
+
+        n_fonts = 0
+        for file_name in os.listdir(font_dir):
+            if re.match("font_\d", file_name):
+                n_fonts += 1
+
+        assets_file.write(struct.pack("<I", n_fonts))
+
+        for font_no in range(n_fonts):
+            cur_font_dir = os.path.join(font_dir, "font_%d" % font_no)
+            write_font(assets_file, cur_font_dir=cur_font_dir)
+
+    shader_offsets = []
+    for shader_idx in range(SHADER_COUNT):
+        shader_offsets.append(assets_file.tell())
+
+        vert_path = os.path.join(shader_dir, "shader_%d_vert.glsl" % shader_idx)
+        frag_path = os.path.join(shader_dir, "shader_%d_frag.glsl" % shader_idx)
+
+        write_text(assets_file, text_file_path=vert_path)
+        write_text(assets_file, text_file_path=frag_path)
+
+    file_offsets = []
+    for file_idx in range(FILE_COUNT):
+        file_offsets.append(assets_file.tell())
+
+        file_path = os.path.join(file_dir, "file_%d.txt" % file_idx)
+        write_text(assets_file, text_file_path=file_path)
+
+    # read in the type sizes
+    type_sizes = []
+    type_size_file = open(type_sizes_path, "r")
+    type_size_txt = type_size_file.read().splitlines()
+    for i in range(TYPE_SIZE_COUNT):
+        ts = int(type_size_txt[i], 0)
+        type_sizes.append(ts)
+
+    # now write the offsets block and the type sizes
+    assets_file.seek(OFFSETS_START, os.SEEK_SET)
+    for offset in (img_offsets + sound_offsets + font_offsets + \
+                   shader_offsets + file_offsets + type_sizes):
+        assets_file.write(struct.pack("<I", offset))
+
 def extract_all_assets(assets_file_path, assets_dir_path):
+    if os.path.exists(assets_dir_path):
+        print "Error: \"%s\" already exists" % assets_dir_path
+        exit(1)
+
     init_paths(assets_dir_path)
     assets_file = open(assets_file_path, "rb")
 
@@ -192,30 +355,36 @@ def extract_all_assets(assets_file_path, assets_dir_path):
     preload_data_file.write(preload_data)
 
     # read in image offsets
+    img_offsets = []
     for i in range(IMG_COUNT):
         offset = struct.unpack("<I", assets_file.read(4))[0]
         img_offsets.append(offset)
 
     # read in sound offsets
+    sound_offsets = []
     for i in range(SOUND_COUNT):
         offset = struct.unpack("<I", assets_file.read(4))[0]
         sound_offsets.append(offset)
 
     # read in the font offsets
+    font_offsets = []
     for i in range(FONT_COUNT):
         offset = struct.unpack("<I", assets_file.read(4))[0]
         font_offsets.append(offset)
 
     # read in the shader offsets
+    shader_offsets = []
     for i in range(SHADER_COUNT):
         offset = struct.unpack("<I", assets_file.read(4))[0]
         shader_offsets.append(offset)
 
     # read in the file offsets
+    file_offsets = []
     for i in range(FILE_COUNT):
         offset = struct.unpack("<I", assets_file.read(4))[0]
         file_offsets.append(offset)
 
+    type_sizes = []
     for i in range(TYPE_SIZE_COUNT):
         ts = struct.unpack("<I", assets_file.read(4))[0]
         type_sizes.append(ts)
@@ -269,20 +438,38 @@ def extract_all_assets(assets_file_path, assets_dir_path):
         extract_text(assets_file, os.path.join(assets_dir_path, "files", \
                                                "file_%d.txt" % index))
 if __name__ == "__main__":
+    do_extract = False
+    do_compress = False
     try:
-        for option, value in \
-            getopt(sys.argv[1:], "f:d:", ["in-file=", "out-dir="])[0]:
+        opt_val, params = getopt(sys.argv[1:], "xcf:", ["file="])
+        for option, value in opt_val:
             if option == "-f" or option == "--in-file":
                 assets_file_path = value
-            elif option == "-d" or option == "--out-dir":
-                assets_dir_path = value
+            elif option == "-c":
+                do_compress = True
+            elif option == "-x":
+                do_extract = True
     except GetoptError:
         print usage_string
         exit(1)
 
-    if os.path.exists(assets_dir_path):
-        print "Error: \"%s\" already exists" % assets_dir_path
+    if len(params) == 1:
+        assets_dir_path = params[0]
+    elif len(params) != 0:
+        print "Error: extra unparsed arguments: %s" % str(params)
         exit(1)
 
-    extract_all_assets(assets_file_path=assets_file_path, \
-                       assets_dir_path=assets_dir_path)
+    if not (do_compress or do_extract):
+        print "Error: need to specify either compress (-c) or extract (-x)"
+        exit(1)
+
+    if do_compress and do_extract:
+        print "Error: cannot both compress (-c) and extract (-x) at the same time"
+        exit(1)
+
+    if do_extract:
+        extract_all_assets(assets_file_path=assets_file_path, \
+                           assets_dir_path=assets_dir_path)
+
+    if do_compress:
+        write_assets_file(assets_file_path, assets_dir_path)
